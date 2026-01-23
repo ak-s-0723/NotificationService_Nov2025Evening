8d289ec30d4dc2024ab21f5744e950e7a5c18ffe
khannaanurag1 <khannaanurag1@gmail.com>
2026-01-23 23:37:25 +0530

    kafka integration

---
+++ src/main/java/org/example/emailservice_nov2025evening/EmailServiceNov2025EveningApplication.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_nov2025evening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceNov2025EveningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceNov2025EveningApplication.class, args);
+    }
+
+}
